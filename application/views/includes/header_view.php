ebb72fe8ca09f5fc33ce1f7140d4b5307189e062
Shawn Boyle <sboyle92@gmail.com>
2013-10-07 10:08:01 -0400

    created the htaccess rule and got the mysql query to work right so now the query will pull the data and push it to the screen correctly

---
+++ application/views/includes/header_view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+
+<link href="http://fonts.googleapis.com/css?family=Muli" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Mako" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="/meadhall/css/meadhall.css">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Meadhall</title>
+
+</head>
+
+<body>
+
+<div id="big_container">
+	<div id="spacer"></div>
+	<div id="top">
+	  <div id="information">
+		<span class="info" id="top_info">
+			4 Cambridge Center<br>
+			Cambridge MA 02142<br>
+			617-714-4372<br>
+			11:30am-1:00am<br>
+		</span>
+		</div>
+		<!--
+        <div id="menu-bottle">
+            <div id="menu">
+                <a href="#"><span class="nav">FOOD</span></a>
+                <a href="#"><span class="nav">DRINK</span></a>
+                <a href="#"><span class="nav">ABOUT</span></a>
+            </div>
+        </div>-->
+	</div><img src="/meadhall/images/logo.jpg" id="logo" alt="Meadhall: Established 2011">
+    <div id="sidebar">
+        <img src="/meadhall/images/banner_150w.jpg" id="banner" alt="Meadhall"><br>
+        <img src="/meadhall/images/key.jpg" id="key" alt="Meadhall - Keys">
+    </div>
+    <img src="/meadhall/images/taps.jpg" class="photo" alt="Meadhall - Taps">
