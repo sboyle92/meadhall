63c33b8083d456ea5ca5a6517ccb0f9ed2ecd909
Shawn Boyle <sboyle92@gmail.com>
2013-11-07 15:10:32 -0500

    modified the database, added DELETE CASCADE to the mBeers table. had to change the view page for this as well

--- application/views/beers_view_list.php
+++ application/views/beers_view_list.php
@@ -4,25 +4,27 @@
     		<table>
 			<?php 
 				$catName = '';
-	 			foreach($beers as $b) 
-	 			{
-					if($b->c_parent_id == 0)
+				foreach($beers as $b) 
+				{
+					if($b->parent_id == 0)
 					{
-						echo '<tr><td colspan="7"><h2>'.$b->c_name .'</h2></td></tr>';
+						echo '<tr><th colspan="7"> <h2>'.$b->cat_name .'</h2></th></tr>';
 					} 
 						else 
 					{
-						if($catName =='' || $catName != $b->c_name)
+						if($catName =='' || $catName != $b->cat_name)
 						{	
-							$catName = $b->c_name;
-							echo '<tr><td colspan="7"><h3>'.$b->c_name.'</h3></td></tr>';
+							$catName = $b->cat_name;
+							echo '<tr><th colspan="7"><h3>'.$b->cat_name.'</h3></th></tr>';
 							echo '<tr><td>Name</td><td>Brewery</td><td>Origin</td><td>% ABV</td><td>Pour</td><td>Price</td><td>Description</td></tr>';
-							echo '<tr><td>'.$b->b_name.' </td><td>'.$b->br_name.' </td><td>'.$b->br_origin.' </td><td> '. $b->b_abv .'% </td><td> '. $b->b_pour.' </td><td>$'. $b->b_price.'</td><td>'. $b->b_description.'</td></tr>';
-						} else {
-							echo '<tr><td>'.$b->b_name.' </td><td>'.$b->br_name.' </td><td>'.$b->br_origin.' </td><td> '. $b->b_abv .'% </td><td> '. $b->b_pour.' </td><td>$'. $b->b_price.'</td><td>'. $b->b_description.'</td></tr>';
+							echo '<tr><td>'.$b->name.' </td><td> '.$b->br_name .'</td><td>'.$b->br_origin.' </td><td> '. $b->abv .'% </td><td> '. $b->pour.' </td><td>$'. $b->price.'</td><td>'. $b->description.'</td></tr>';
+						} 
+							else 
+						{
+							echo '<tr><td>'.$b->name .'</td><td>'.$b->br_name .'</td><td>'.$b->br_origin.' </td><td> '. $b->abv .'% </td><td> '. $b->pour.' </td><td> $'. $b->price.'</td><td>'. $b->description.'</td></tr>';
 						}
 					}
-	 			}
+				}
 			?>
     		</table>
     	</div>
